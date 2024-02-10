09bff3beb4fca68f2ab47d99f24ff8baf7269f3d
Cybernetics <mail@cybernetics.me>
2024-02-10 09:45:27 +0530

    small updates

---
+++ resources/views/base.blade.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <!-- title -->
+    <title>POFO – Creative Agency, Corporate and Portfolio Multi-purpose Template</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <meta name="author" content="ThemeZaa">
+    <!-- description -->
+    <meta name="description" content="POFO is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template with 25 ready home page demos.">
+    <!-- keywords -->
+    <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal, masonry, grid, coming soon, faq">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="{{ asset('/assets/images/favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('/assets/images/apple-touch-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/assets/images/apple-touch-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/assets/images/apple-touch-icon-114x114.png') }}">
+    <!-- style sheets and font icons  -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/bootsnav.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/font-icons.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/theme-vendors.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/style.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/responsive.css') }}" />
+</head>
+
+<body class="w-100">
+    <!-- start header -->
+    <header class="no-sticky">
+        <!-- start navigation -->
+        <nav class="navbar full-width-pull-menu navbar-top-scroll no-border-top nav-box-width no-transition">
+            <!-- start navigation panel -->
+            <div class="container-fluid nav-header-container h-100px sm-h-70px">
+                <!-- start logo -->
+                <div class="col-auto ps-0"><a href="{{ route('index') }}" title="PRO House" class="logo"><img src="{{ asset('/assets/images/prohouse/logo/logo-prohouse-black.webp') }}" data-at2x="{{ asset('/assets/images/prohouse/logo/logo-prohouse-black.webp') }}" alt="PRO House"></a></div>
+                <!-- end logo -->
+                <!-- start main menu -->
+                <div class="col-auto pe-0">
+                    <div class="menu-wrap full-screen">
+                        <div class="row g-0 h-100 justify-content-center">
+                            <div class="col-md-6 p-0 text-center z-index-0 d-none d-md-flex flex-column justify-content-center cover-background" style="background-image:url('{{ asset('/assets/images/menu-background-1.png') }}')">
+                                <div class="z-index-minus1 opacity-medium bg-extra-dark-gray"></div>
+                                <a href="{{ route('index') }}"><img alt="" src="{{ asset('/assets/images/prohouse/logo/logo-prohouse-white.webp') }}" data-at2x="{{ asset('/assets/images/prohouse/logo/logo-prohouse-white.webp') }}"></a>
+                                <div class="position-absolute bottom-50 text-center w-100 margin-30px-bottom">
+                                    <div class="text-small text-extra-medium-gray">&copy; {{ date('Y') }} PRO House</div>
+                                </div>
+                            </div>
+                            @include("nav")
+                        </div>
+                    </div>
+                    <!-- end main menu -->
+                    <button class="navbar-toggler mobile-toggle" type="button" id="open-button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
+            </div>
+            <!-- end navigation panel -->
+        </nav>
+        <!-- end navigation -->
+    </header>
+    <!-- end header -->
+    @yield("content")
+    <!-- start footer -->
+    <footer class="footer-strip border-color-extra-light-gray border-top padding-50px-tb sm-padding-30px-tb">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- start logo -->
+                <div class="col-md-3 text-center text-lg-start sm-margin-20px-bottom">
+                    <a href="index.html"><img class="footer-logo" src="{{ asset('/assets/images/logo.png') }}" data-at2x="{{ asset('/assets/images/logo@2x.png') }}" alt="Pofo"></a>
+                </div>
+                <!-- end logo -->
+                <!-- start copyright -->
+                <div class="col-md-6 text-center text-small alt-font sm-margin-10px-bottom">
+                    &copy; 2024 POFO is Proudly Powered by <a href="http://www.themezaa.com" target="_blank" title="ThemeZaa">ThemeZaa</a>.
+                </div>
+                <!-- end copyright -->
+                <!-- start social media -->
+                <div class="col-md-3 text-center text-lg-end">
+                    <div class="social-icon-style-8 d-inline-block align-middle">
+                        <ul class="small-icon mb-0">
+                            <li><a class="facebook" href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-f" aria-hidden="true"></i></a></li>
+                            <li><a class="twitter" href="https://twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+                            <li><a class="google" href="https://plus.google.com" target="_blank"><i class="fa-brands fa-google-plus-g"></i></a></li>
+                            <li><a class="dribbble" href="https://dribbble.com/" target="_blank"><i class="fa-brands fa-dribbble me-0" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- end social media -->
+            </div>
+        </div>
+    </footer>
+    <!-- end footer -->
+    <!-- start scroll to top -->
+    <a class="scroll-top-arrow" href="javascript:void(0);"><i class="ti-arrow-up"></i></a>
+    <!-- end scroll to top  -->
+    <!-- javascript -->
+    <script type="text/javascript" src="{{ asset('/assets/js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/assets/js/bootsnav.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/assets/js/jquery.nav.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/assets/js/hamburger-menu.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/assets/js/theme-vendors.min.js') }}"></script>
+    <!-- setting -->
+    <script type="text/javascript" src="{{ asset('/assets/js/main.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/nav.blade.php
@@ -0,0 +1,108 @@
+<div class="col-md-6 px-0 bg-white full-screen d-flex flex-column justify-content-center xs-padding-45px-tb">
+    <div class="w-100 overflow-auto">
+        <div class="h-100 w-100">
+            <div class="h-100 w-100 align-middle padding-eighteen-lr alt-font link-style-2 lg-padding-fourteen-lr xs-padding-15px-lr">
+                <!-- start menu -->
+                <ul class="font-weight-600 no-padding-left">
+                    <!-- start menu item -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);">Home</a><span data-bs-toggle="dropdown" class="dropdown-toggle"></span>
+                        <ul class="dropdown-menu font-weight-300">
+                            <li><a href="index.html">Classic corporate</a></li>
+                            <li><a href="home-creative-studio.html">Creative studio</a></li>
+                            <li><a href="home-portfolio-minimal.html">Portfolio minimal</a></li>
+                            <li><a href="home-blog-personal.html">Blog personal</a></li>
+                        </ul>
+                    </li>
+                    <!-- end menu item -->
+                    <!-- start menu item -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);">Pages</a><span data-bs-toggle="dropdown" class="dropdown-toggle"></span>
+                        <ul class="dropdown-menu font-weight-300">
+                            <li><a href="about-us-simple.html">About</a></li>
+                            <li><a href="services-simple.html">Services</a></li>
+                            <li><a href="contact-us-simple.html">Contact us</a></li>
+                            <li><a href="team-simple.html">Team</a></li>
+                            <li><a href="404.html">Additional pages</a></li>
+                        </ul>
+                    </li>
+                    <!-- end menu item -->
+                    <!-- start menu item -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);">Portfolio</a><span data-bs-toggle="dropdown" class="dropdown-toggle"></span>
+                        <ul class="dropdown-menu font-weight-300">
+                            <li><a href="portfolio-full-width-masonry-overlay.html">Portfolio masonry</a></li>
+                            <li><a href="portfolio-full-width-metro-overlay.html">Portfolio metro</a></li>
+                            <li><a href="portfolio-full-width-image-gallery.html">Portfolio image gallery</a></li>
+                            <li><a href="portfolio-boxed-grid-overlay.html">Portfolio grid</a></li>
+                            <li><a href="single-project-page-01.html">Single project page</a></li>
+                            <li><a href="portfolio-two-columns.html">Portfolio columns</a></li>
+                        </ul>
+                    </li>
+                    <!-- end menu item -->
+                    <!-- start menu item -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);">Blog</a><span data-bs-toggle="dropdown" class="dropdown-toggle"></span>
+                        <ul class="dropdown-menu font-weight-300">
+                            <li><a href="blog-standard-full-width.html">Blog standard – full width</a></li>
+                            <li><a href="blog-classic.html">Blog classic</a></li>
+                            <li><a href="blog-list.html">Blog list</a></li>
+                            <li><a href="blog-grid.html">Blog grid</a></li>
+                            <li><a href="blog-masonry.html">Blog masonry</a></li>
+                            <li><a href="blog-simple.html">Blog simple</a></li>
+                            <li><a href="blog-clean.html">Blog clean</a></li>
+                            <li><a href="blog-images.html">Blog images</a></li>
+                            <li><a href="blog-only-text.html">Blog only text</a></li>
+                            <li><a href="blog-post-layout-01.html">Blog post layout 01</a></li>
+                            <li><a href="blog-standard-post.html">Standard post</a></li>
+                            <li><a href="blog-gallery-post.html">Gallery post</a></li>
+                            <li><a href="blog-slider-post.html">Slider post</a></li>
+                            <li><a href="blog-html5-video-post.html">HTML5 video post</a></li>
+                            <li><a href="blog-youtube-video-post.html">Youtube video post</a></li>
+                            <li><a href="blog-vimeo-video-post.html">Vimeo video post</a></li>
+                            <li><a href="blog-audio-post.html">Audio post</a></li>
+                            <li><a href="blog-blockquote-post.html">Blockquote post</a></li>
+                            <li><a href="blog-full-width-post.html">Full width post</a></li>
+                        </ul>
+                    </li>
+                    <!-- end menu item -->
+                    <!-- start menu item -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);">Elements</a><span data-bs-toggle="dropdown" class="dropdown-toggle"></span>
+                        <ul class="dropdown-menu font-weight-300">
+                            <li><a href="accordions.html">General elements</a></li>
+                            <li><a href="progress-bar.html">Infographics / icons</a></li>
+                            <li><a href="testimonials.html">Interactive elements</a></li>
+                            <li><a href="heading.html">Text & containers</a></li>
+                        </ul>
+                    </li>
+                    <!-- end menu item -->
+                    <!-- start menu item -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);">Features</a><span data-bs-toggle="dropdown" class="dropdown-toggle"></span>
+                        <ul class="dropdown-menu font-weight-300">
+                            <li><a href="transparent-header.html">Header styles</a></li>
+                            <li><a href="footer-standard.html">Footer</a></li>
+                            <li><a href="page-title-left-alignment.html">Page title</a></li>
+                            <li><a href="single-image-lightbox.html">Gallery</a></li>
+                        </ul>
+                    </li>
+                    <!-- end menu item -->
+                </ul>
+                <!-- end menu -->
+                <!-- start social links -->
+                <div class="margin-fifteen-top">
+                    <div class="icon-social-medium padding-three-bottom">
+                        <a href="https://www.facebook.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa-brands fa-facebook-f" aria-hidden="true"></i></a>
+                        <a href="https://twitter.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa-brands fa-twitter" aria-hidden="true"></i></a>
+                        <a href="https://dribbble.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa-brands fa-dribbble" aria-hidden="true"></i></a>
+                        <a href="https://plus.google.com" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa-brands fa-google-plus-g" aria-hidden="true"></i></a>
+                        <a href="https://www.tumblr.com/" target="_blank" class="text-extra-dark-gray text-deep-pink-hover margin-one-lr"><i class="fa-brands fa-tumblr" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <!-- start social links -->
+            </div>
+        </div>
+    </div>
+    <button class="close-button-menu" id="close-button"></button>
+</div>
